c2af5b1dfd22e80213b7fd8cf663f955c6d9a530
joseLopez <kota012011@gmail.com>
2025-05-10 10:08:54 -0500

    condicionales por ROLES

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -1,1 +1,9 @@
-<H1>saludos desde index products</H1>
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Products') }}
+        </h2>
+    </x-slot>
+
+   
+</x-app-layout>
